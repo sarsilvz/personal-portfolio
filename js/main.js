434b21e7d0c246421c36d7d26b92895e04a7831d
Sarah Silva <sarah@Sarah-Silvas-MacBook-Pro.local>
2017-06-28 16:35:41 -0400

    added image gallery to mobile. next fix button function

--- js/main.js
+++ js/main.js
@@ -3,24 +3,33 @@ $('h3').click(function() {
   console.log("boom");
 });
 
-// -- //
 
 
+$('.circleimg').first().addClass('shown');
 
 
-$(".more").click(function () {
+var nextcircleimg = function () {
 
-    $more = $(this);
-    //getting the next element
-    $morebox = more.next();
-    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
-    $morebox.slideToggle(500, function () {
-        //execute this after slideToggle is done
-        //change text of header based on visibility of content div
-        $more.text(function () {
-            //change text based on condition
-            return $morebox.is(":visible") ? "Collapse" : "Expand";
-        });
-    });
+  if ($('.shown').is(':last-child')) {
+    return;
+  }
 
-});
+  else {
+    $('.shown').removeClass('shown').next('.circleimg').addClass('shown');
+  }
+}
+
+var prevcircleimg = function () {
+
+  if ($('.shown').is(':first-child')) {
+    return;
+  }
+
+  else {
+    $('.shown').removeClass('shown').previous('.circleimg').addClass('shown');
+  }
+}
+
+
+$('.next').click(nextcircleimg);
+$('.prev').click(prevcircleimg);
